be13cb1fedfd140449e52a5806d3571d73b6131e
sidhart25 <navneetnishchal1420@gmail.com>
2024-04-14 23:57:20 +0530

    folder structure almost done and started

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,9 @@
+import { HomeOne, NavBar } from "./comp";
 export default function Home() {
   return (
-    <div className="border-4 border-red-500  flex h-dvh flex items-center justify-center">
-      <div className="text-3xl text-sky-500 border-4 border-green-600 h-1/4 w-3/12  align-middle content-center text-center">
-        My Home
-      </div>
+    <div className="flex h-dvh w-dvw border-4 border-green-700">
+      <NavBar />
+      <HomeOne />
     </div>
   );
 }
