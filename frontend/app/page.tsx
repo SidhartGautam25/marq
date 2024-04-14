7971ec34409065998f330eace7963facfb0e1217
Ankit <ankitdj13112002@gmail.com>
2024-04-15 02:23:17 +0530

    navbar

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,9 @@
 import { HomeOne, NavBar } from "./comp";
 export default function Home() {
   return (
-    <div className="flex h-dvh w-dvw border-4 border-green-700">
+    <div className="flex flex-col h-dvh w-dvw">
       <NavBar />
-      <HomeOne />
+      {/* <HomeOne /> */}
     </div>
   );
 }
