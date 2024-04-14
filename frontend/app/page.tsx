efcde2c61d4d04c988b1fbecb35b8c4b076fcb5d
Ankit <ankitdj13112002@gmail.com>
2024-04-15 00:12:12 +0530

    i am doing something

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl">
-      My Home
-    </h1>
+    <div className="border-red-400  h-dvh border-4 flex items-center justify-center">
+      <div className="text-3xl border-green-400 border-4 h-full w-5/6 text-center content-center">
+        hello world
+      </div>
+    </div>
   )
 }
