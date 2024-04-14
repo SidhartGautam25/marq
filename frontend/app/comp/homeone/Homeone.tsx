10da0b38d2b42ae988631196f1feeafcd82d7422
sidhart25 <navneetnishchal1420@gmail.com>
2024-04-15 04:53:18 +0530

    navbar and first comp completed

--- frontend/app/comp/homeone/Homeone.tsx
+++ frontend/app/comp/homeone/Homeone.tsx
@@ -2,31 +2,35 @@
 
 export default function HomeOne() {
   return (
-    <div className="flex border-2 border-red-600 h-[90vh] w-full">
-      <div className="w-[45vw]">hello</div>
-      <div className="w-[55vw] border-2  flex flex-col justify-center">
-        <div className="text-6xl font-bold mb-4 font-serif">
+    <div className="flex h-[86vh] w-full bg-img1 z-10">
+      <div className="w-[45vw] "></div>
+      <div className="w-[55vw]   flex flex-col justify-center">
+        <div className="text-[3rem] font-bold mb-3 font-serif text-white">
           Helping You Think{" "}
         </div>
-        <div className="text-6xl font-bold mb-4 font-serif">
+        <div className="text-[3rem] font-bold mb-3 font-serif  text-white">
           Strategically & Execute
         </div>
-        <div className="text-6xl font-bold mb-4 font-serif">Brilliantly.</div>
+        <div className="text-[3rem] font-bold mb-3 font-serif  text-white">
+          Brilliantly.
+        </div>
         <div>
-          <div className="mt-2 mb-1 font-semibold text-lg">
+          <div className="mt-2 mb-1 font-semibold text-lg  text-white">
             Marqstats is focused on delivering exceptional client service and
             expertise to
           </div>
-          <div className="mt-1 mb-1 font-semibold text-lg">
+          <div className="mt-1 mb-1 font-semibold text-lg  text-white">
             accelerate progress.{" "}
           </div>
         </div>
         <div>
-          <div className="mt-2 mb-1 font-semibold text-lg">
+          <div className="mt-2 mb-1 font-semibold text-lg  text-white">
             What’s the result? Transformed experiences, decisive actions and
             long-term value for
           </div>
-          <div className="mt-1 font-semibold text-lg">all stakeholders.</div>
+          <div className="mt-1 font-semibold text-lg  text-white">
+            all stakeholders.
+          </div>
         </div>
         <div className="flex mt-7 p-2  rounded-md border-2 border-black bg-black hover:bg-white hover:text-black text-white text-base w-fit">
           LET'S WORK TOGETHER{" "}
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,18 @@
 import { HomeOne, NavBar } from "./comp";
+import img1 from "../public/images/img1.jpg";
+import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex flex-col h-dvh w-dvw">
-      <NavBar />
-      {/* <HomeOne /> */}
+      <div className="flex flex-col h-dvh w-dvw">
+        <Image
+          src={img1}
+          alt=""
+          className="opacity-100 brightness-[0.3] z-0 absolute w-full h-full"
+        />
+        <NavBar />
+        <HomeOne />
+      </div>
     </div>
   );
 }
