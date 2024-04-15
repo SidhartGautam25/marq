82b2e02fd7bbd9f0e346bb78eb5658bc17c0ec20
sidhart25 <navneetnishchal1420@gmail.com>
2024-04-15 07:59:14 +0530

    first component is integrated

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,18 +1,27 @@
-
-import { HomeOne, NavBar } from "./comp";
+import { HomeOne, NavBar, Footer } from "./comp";
 import img1 from "../public/images/img1.jpg";
 import Image from "next/image";
 export default function Home() {
   return (
-    <div className="flex flex-col h-dvh w-dvw">
-      <div className="flex flex-col h-dvh w-dvw">
-        <Image
-          src={img1}
-          alt=""
-          className="opacity-100 brightness-[0.3] z-0 absolute w-full h-full"
-        />
-        <NavBar />
-        <HomeOne />
+    <div className="flex flex-col w-dvw">
+      <div className="h-dvh w-dvw ">
+        <div className="relative w-dvw">
+          <div className="absolute top-0 left-0 z-0">
+            <Image
+              src={img1}
+              alt=""
+              className="object-fill h-dvh w-dvw brightness-[0.5]"
+            />
+          </div>
+        </div>
+
+        <div className="absolute z-10">
+          <NavBar />
+          <HomeOne />
+        </div>
+      </div>
+      <div className="">
+        <Footer />
       </div>
     </div>
   );
