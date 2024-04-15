b360b01dc4d9a46cf0425be33cd85939b97c778c
Ankit <ankitdj13112002@gmail.com>
2024-04-15 07:41:45 +0530

    slider

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,12 @@
-import { Footer, HomeOne, NavBar } from "./comp";
+import { Footer, HomeOne, NavBar, Slid, Vision } from "./comp";
 export default function Home() {
   return (
     <div className="flex flex-col h-dvh w-dvw">
       <NavBar />
       {/* <HomeOne /> */}
-      <Footer/>
+      {/* <Footer/> */}
+      {/* <Slid/> */}
+      <Vision/>
     </div>
   );
 }
