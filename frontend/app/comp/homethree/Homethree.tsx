69ced6be2d80abb5459bd4efa68a2a024a54c21f
isham-26 <ishamgupta711@gmail.com>
2024-04-16 02:40:06 +0530

    Homethree completed

---
+++ frontend/app/comp/homethree/Homethree.tsx
@@ -0,0 +1,89 @@
+export default function Homethree() {
+  return (
+    <>
+      <div className="w-full my-10 p-10 flex justify-center items-center">
+        <div className="main w-[85%] flex gap-20">
+          <div className="left w-1/2 p-5">
+            <p className=" mb-16 text-3xl font-bold leading-[50px]">
+              The global market is defined by its complexity and fluidity.
+              That's where we come in.Marqstats offers a range of solutions to
+              support your goal.
+            </p>
+            <button className="btn-blue text-lg flex items-center border-2 border-blue-500 p-3 hover:bg-blue-500 text-blue-500 hover:text-white">
+              VIEW ALL SERVICES
+              <svg
+                className="ml-2 w-6 h-6"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path d="M5 12h14M12 5l7 7-7 7"></path>
+              </svg>
+            </button>
+          </div>
+          <div className="right w-1/2 flex flex-col gap-2">
+            <div className="strap  flex justify-between items-center p-5 ease-in duration-400 hover:border-b border-blue-500">
+                <span className="text-3xl">Industry Expertise</span>
+                <svg
+                className="ml-2 w-7 h-7"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path d="M5 12h14M12 5l7 7-7 7"></path>
+              </svg>
+            </div>
+            <div className="strap  flex justify-between items-center p-5 ease-in duration-400 hover:border-b border-blue-500">
+                <span className="text-3xl">Market Assessment</span>
+                <svg
+                className="ml-2 w-7 h-7"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path d="M5 12h14M12 5l7 7-7 7"></path>
+              </svg>
+            </div>
+            <div className="strap  flex justify-between items-center p-5 ease-in duration-400 hover:border-b border-blue-500">
+                <span className="text-3xl">Data Resource</span>
+                <svg
+                className="ml-2 w-7 h-7"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path d="M5 12h14M12 5l7 7-7 7"></path>
+              </svg>
+            </div>
+            <div className="strap  flex justify-between items-center p-5 ease-in duration-400 hover:border-b border-blue-500">
+                <span className="text-3xl">Business Consulting</span>
+                <svg
+                className="ml-2 w-7 h-7"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path d="M5 12h14M12 5l7 7-7 7"></path>
+              </svg>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- frontend/app/comp/index.ts
+++ frontend/app/comp/index.ts
@@ -7,6 +7,7 @@ import Vision from "./vision/Vision";
 import Homefour from "./homefour/Homefour";
 import Homesix from "./homesix/Homesix";
 import Homefive from "./homefive/Homefive";
+import Homethree from "./homethree/Homethree";
 export {
   
   NavBar,
@@ -15,5 +16,11 @@ export {
   Slid,
   Vision,
   Homefive,
+  Homefour,
+  Homesix,
+  Homethree
+
+
+
 
 };
