93a3cd516c898b72d01dea38f348e5e984b3eaff
sidhart25 <navneetnishchal1420@gmail.com>
2024-04-17 11:36:58 +0530

    now everything is done and fine

--- frontend/app/comp/navbar/Navbar.tsx
+++ frontend/app/comp/navbar/Navbar.tsx
@@ -12,7 +12,7 @@ export default function NavBar() {
 
   return (
     <nav className="z-[10] border-gray-500 border-t-[0.1px] border-b-[0.1px] min-h-[14vh] p-4 lg:p-6 lg:flex w-full">
-      <div className="flex items-center justify-between ml-20 ">
+      <div className="flex items-center justify-between lg:ml-20 ">
         <Link href="/">
           <div className="tracking-tight ">
             <Image src={logo} alt="" className=" h-16 w-36" />
