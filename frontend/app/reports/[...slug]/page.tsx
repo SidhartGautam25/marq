be9e221007f0e00ec27d4ac228921e1a16927df4
Ankit <ankitdj13112002@gmail.com>
2024-05-22 13:49:10 +0530

    devlopment folder

---
+++ frontend/app/reports/[...slug]/page.tsx
@@ -0,0 +1,16 @@
+import { Footer, NavBar, Relatedarticle, RightHero } from "../../comp";
+export default function Page() {
+  return (
+    <div className="">
+      <div className=" bg-gray-900">
+        <NavBar />
+      </div>
+      <h1>Slug part</h1>
+      <div className="flex">
+        <RightHero />
+        <Relatedarticle />
+      </div>
+      <Footer />
+    </div>
+  );
+}
