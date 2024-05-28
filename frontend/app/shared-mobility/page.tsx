4ec87310897e41d3a3e2b39ca45888b9b0e1f60a
sidhart25 <navneetnishchal1420@gmail.com>
2024-05-29 00:38:36 +0530

    some imp again

--- frontend/app/shared-mobility/page.tsx
+++ frontend/app/shared-mobility/page.tsx
@@ -1,19 +1,19 @@
-import ele from "@/public/assets/Shared-Mobility.jpg";
-import Report from "../comp/report/Report"
-import Industry from "../comp/industry/Industry";
-import { NavBar } from "../comp";
-export default function Page() {
-  let str="Shared mobility is transforming transportation by offering shared services over individual vehicle ownership. This encompasses ride-hailing, car-sharing, and subscription models, driven by urbanization, evolving consumer choices, and sustainable transit needs. Technology is a key enabler, linking users to vehicles via apps. Delve into shared mobility's expansion, novel business approaches, and influence on conventional transportation norms."
-  let head="Shared Mobility";
-  return (
-    <>
-      <div className="bg-gray-900">
-     <NavBar/>
-      <div className="main bg-gray-100">
-        <Industry imgs={ele.src} para={str} heading={head}/>
-        <Report/>
-      </div>
-    </div>
-    </>
-  );
-}
+// import ele from "@/public/assets/Shared-Mobility.jpg";
+// import Report from "../comp/report/Report"
+// import Industry from "../comp/industry/Industry";
+// import { NavBar } from "../comp";
+// export default function Page() {
+//   let str="Shared mobility is transforming transportation by offering shared services over individual vehicle ownership. This encompasses ride-hailing, car-sharing, and subscription models, driven by urbanization, evolving consumer choices, and sustainable transit needs. Technology is a key enabler, linking users to vehicles via apps. Delve into shared mobility's expansion, novel business approaches, and influence on conventional transportation norms."
+//   let head="Shared Mobility";
+//   return (
+//     <>
+//       <div className="bg-gray-900">
+//      <NavBar/>
+//       <div className="main bg-gray-100">
+//         <Industry imgs={ele.src} para={str} heading={head}/>
+//         <Report/>
+//       </div>
+//     </div>
+//     </>
+//   );
+// }
