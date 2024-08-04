54246b78e737d809637ca28c37f36918622c5b69
sidhart25 <navneetnishchal1420@gmail.com>
2024-08-05 05:23:22 +0530

    context for submit state added

--- frontend/app/comp/report-card/Card.tsx
+++ frontend/app/comp/report-card/Card.tsx
@@ -25,7 +25,7 @@ const Card: React.FC<RepProps> = ({ rep }) => {
       type: "SET_CURRENT",
       payload: rep,
     });
-    let temp = rep.title.replace(/\s/g, "-");
+    let temp = rep.slug.replace(/\s/g, "-");
     router.push(`/report/${temp}`);
   }
 
