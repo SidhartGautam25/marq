5175d0f3019716e4598ececeb0b8901015034bf4
sidhart25 <navneetnishchal1420@gmail.com>
2024-08-06 21:56:04 +0530

    smooth scroll added

--- frontend/app/comp/report-order/OrderComponent.tsx
+++ frontend/app/comp/report-order/OrderComponent.tsx
@@ -1,3 +1,76 @@
+// import { ReportContext, ReportContextType } from "@/app/context/reportContext";
+// import React, { useContext, useState } from "react";
+
+// const OrderComponent: React.FC = () => {
+//   const [selectedOption, setSelectedOption] = useState<number | null>(null);
+//   const { state, dispatch } = useContext(ReportContext) as ReportContextType;
+
+//   const handleOptionClick = (option: number) => {
+//     setSelectedOption(option === selectedOption ? null : option);
+//   };
+
+//   return (
+//     <div className="max-w-md mx-auto shadow-md bg-gray-50 p-2">
+//       <h2 className="text-xl font-semibold mb-4 text-center">Place An Order</h2>
+//       <div>
+//         <div
+//           className={` mb-2 border rounded cursor-pointer  ${
+//             selectedOption === 1
+//               ? "bg-sky-950 text-white"
+//               : "bg-gray-700/10 text-black"
+//           }`}
+//           onClick={() => handleOptionClick(1)}
+//         >
+//           <div className=" p-2">1. Data Only (MS Excel) : ${state.pSingle}</div>
+//           {selectedOption === 1 && (
+
+//             <div className="mt-2 text-sm bg-white text-gray-600 p-2 ">
+//               Access market data conveniently organized in an Excel spreadsheet
+//               for data-driven insights
+//             </div>
+//           )}
+//         </div>
+//         <div
+//           className={` mb-2 border rounded cursor-pointer  ${
+//             selectedOption === 2
+//               ? "bg-sky-950 text-white"
+//               : "bg-gray-700/10 text-black"
+//           }`}
+//           onClick={() => handleOptionClick(2)}
+//         >
+//           <div className="p-2">2. Full Report (PDF) : ${state.pTeam}</div>
+//           {selectedOption === 2 && (
+//             <div className="mt-2 text-sm bg-white text-gray-600 p-2">
+//               Gain comprehensive market research findings and analysis in a
+//               visually appealing PDF format.
+//             </div>
+//           )}
+//         </div>
+//         <div
+//           className={` mb-2 border rounded cursor-pointer ${
+//             selectedOption === 3
+//               ? "bg-sky-950 text-white"
+//               : "bg-gray-700/10 text-black"
+//           }`}
+//           onClick={() => handleOptionClick(3)}
+//         >
+//           <div className=" p-2">
+//             3. Complete - Report + Data : ${state.pCorp}
+//           </div>
+//           {selectedOption === 3 && (
+//             <div className="mt-2 text-sm bg-white text-gray-600 p-2">
+//               Receive both an Excel Data Pack and a PDF Report for a complete
+//               market research solution
+//             </div>
+//           )}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default OrderComponent;
+
 import { ReportContext, ReportContextType } from "@/app/context/reportContext";
 import React, { useContext, useState } from "react";
 
@@ -14,23 +87,27 @@ const OrderComponent: React.FC = () => {
       <h2 className="text-xl font-semibold mb-4 text-center">Place An Order</h2>
       <div>
         <div
-          className={` mb-2 border rounded cursor-pointer  ${
+          className={`mb-2 border rounded cursor-pointer transition-all duration-500 ease-in-out ${
             selectedOption === 1
               ? "bg-sky-950 text-white"
               : "bg-gray-700/10 text-black"
           }`}
           onClick={() => handleOptionClick(1)}
         >
-          <div className=" p-2">1. Data Only (MS Excel) : ${state.pSingle}</div>
-          {selectedOption === 1 && (
+          <div className="p-2">1. Data Only (MS Excel) : ${state.pSingle}</div>
+          <div
+            className={`overflow-hidden transition-all duration-500 ease-in-out ${
+              selectedOption === 1 ? "max-h-40" : "max-h-0"
+            }`}
+          >
             <div className="mt-2 text-sm bg-white text-gray-600 p-2">
               Access market data conveniently organized in an Excel spreadsheet
               for data-driven insights
             </div>
-          )}
+          </div>
         </div>
         <div
-          className={` mb-2 border rounded cursor-pointer  ${
+          className={`mb-2 border rounded cursor-pointer transition-all duration-500 ease-in-out ${
             selectedOption === 2
               ? "bg-sky-950 text-white"
               : "bg-gray-700/10 text-black"
@@ -38,30 +115,38 @@ const OrderComponent: React.FC = () => {
           onClick={() => handleOptionClick(2)}
         >
           <div className="p-2">2. Full Report (PDF) : ${state.pTeam}</div>
-          {selectedOption === 2 && (
+          <div
+            className={`overflow-hidden transition-all duration-500 ease-in-out ${
+              selectedOption === 2 ? "max-h-40" : "max-h-0"
+            }`}
+          >
             <div className="mt-2 text-sm bg-white text-gray-600 p-2">
               Gain comprehensive market research findings and analysis in a
               visually appealing PDF format.
             </div>
-          )}
+          </div>
         </div>
         <div
-          className={` mb-2 border rounded cursor-pointer ${
+          className={`mb-2 border rounded cursor-pointer transition-all duration-500 ease-in-out ${
             selectedOption === 3
               ? "bg-sky-950 text-white"
               : "bg-gray-700/10 text-black"
           }`}
           onClick={() => handleOptionClick(3)}
         >
-          <div className=" p-2">
+          <div className="p-2">
             3. Complete - Report + Data : ${state.pCorp}
           </div>
-          {selectedOption === 3 && (
+          <div
+            className={`overflow-hidden transition-all duration-500 ease-in-out ${
+              selectedOption === 3 ? "max-h-40" : "max-h-0"
+            }`}
+          >
             <div className="mt-2 text-sm bg-white text-gray-600 p-2">
               Receive both an Excel Data Pack and a PDF Report for a complete
               market research solution
             </div>
-          )}
+          </div>
         </div>
       </div>
     </div>
